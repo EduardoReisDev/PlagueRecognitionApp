9c52a3c205fbf1df64a99f50eb99c49e37429fd2
Eduardo Reis <eduardoreisdev@gmail.com>
2022-03-27 16:03:58 -0300

    WIP

---
+++ PlagueRecognition/PlagueRecognition/Model/Prediction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace PlagueRecognition.Model
+{
+    public class Prediction
+    {
+        [JsonProperty("probability")]
+        public double Probability { get; set; }
+
+        [JsonProperty("tagId")]
+        public Guid TagId { get; set; }
+
+        [JsonProperty("tagName")]
+        public string TagName { get; set; }
+    }
+}
