300f5e92cc9905f29c01e58122bf864edfb9b42e
Eduardo Reis <eduardoreisdev@gmail.com>
2022-05-17 21:22:14 -0300

    WIP

--- PlagueRecognition/PlagueRecognition.Android/obj/Debug/android/environment.armeabi-v7a.s
+++ PlagueRecognition/PlagueRecognition.Android/obj/Debug/android/environment.armeabi-v7a.s
@@ -105,7 +105,7 @@ mono_aot_mode_name:
 	.section	.rodata.env.str.8,"aMS",%progbits,1
 	.type	.L.env.str.8, %object
 .L.env.str.8:
-	.asciz	"80f19033-eedd-4172-a4d9-131845d61833"
+	.asciz	"2346fb83-c269-4590-98f0-f11303c069a0"
 	.size	.L.env.str.8, 37
 	.section	.rodata.env.str.9,"aMS",%progbits,1
 	.type	.L.env.str.9, %object
